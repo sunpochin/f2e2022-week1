04df23dff00d6ed58c3607857bc4e4a15c8b0529
pac <sunpochin@gmail.com>
2022-11-22 14:56:10 +0800

    adjusting bg color

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,33 @@
 <template>
-  <HelloWorld msg="F2E week 1 project placeholder"/>
+  <NavBar />
   <HomeViewVue />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
 import HomeViewVue from './views/HomeView.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    NavBar,
     HomeViewVue,
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0px;
+  padding: 0px;
+  background-color: #06102B;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #2c3e50; */
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="outer">
+    <!-- <Logo /> -->
+    <!-- <img src="" alt=""> -->
+    <ul class="xxxxx">
+      <li class="mr-8 li">
+        <a href="">關卡資訊</a>
+      </li>
+      <li class="mr-8 li"><a href="">攻略資源</a></li>
+      <li class="mr-8 li"><a href="">求職專區</a></li>
+      <button class="h-10 border rounded-full w-20 hover:border-accent hover:text-accent">
+        <a href="">登入</a>
+      </button>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+// import Logo from "../image/F2E-Logo.png";
+</script>
+
+<style scoped>
+.outer {
+  display: flex;
+}
+.xxxxx {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  list-style-type: none;
+
+}
+
+li {
+  margin: 8px;
+}
+li a:hover {
+  color: white;
+}
+
+</style>
